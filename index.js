12051a690711186538aa7d29c3979a3d4d2b90f7
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-05-22 17:46:08 +0700

    API - [GET] node

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const db = require('./connect')
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Home Page Route'));
+app.all('/', (req, res) => {
+    res.json({
+        "err": 'path not found. <a href="./">API Document</a>'
+    })
+})
 
-app.get('/about', (req, res) => res.send('About Page Route'));
-
-app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
-
-app.get('/contact', (req, res) => res.send('Contact Page Route'));
+app.get('/node', async(req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+    const node = await db.collection('node').find({}).toArray()
+    res.json({ node })
+});
 
 const port = process.env.PORT || 80;
 
